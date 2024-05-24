b6eba561df94352908c0439dd81df4458022e263
vjimcor955 <vjimcor955@g.educaand.es>
2024-05-24 09:57:15 +0200

    index routes

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+// Index with all the routes
+
+const express = require('express');
+const userRoutes = require('./userRoutes');
+const uploadedVideoRoutes = require('./uploadedVideoRoutes');
+const resultVideoRoutes = require('./resultVideoRoutes');
+
+const router = express.Router();
+
+// Use individual route modules
+router.use('/user', userRoutes);
+router.use('/uploadVideo', uploadedVideoRoutes);
+router.use('/resultVideo', resultVideoRoutes);
+
+module.exports = router;
