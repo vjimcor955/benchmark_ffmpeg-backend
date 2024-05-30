6f10183c2ac47e29ab8467c2416e8f4e216cfbde
vjimcor955 <vjimcor955@g.educaand.es>
2024-05-30 18:07:19 +0200

    import user routes into the main router

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,15 @@
 
 const express = require('express');
 const userRoutes = require('./userRoutes');
-const uploadedVideoRoutes = require('./uploadedVideoRoutes');
-const resultVideoRoutes = require('./resultVideoRoutes');
+// const uploadedVideoRoutes = require('./uploadedVideoRoutes');
+// const resultVideoRoutes = require('./resultVideoRoutes');
 
 const router = express.Router();
 
 // Use individual route modules
 router.use('/user', userRoutes);
-router.use('/uploadVideo', uploadedVideoRoutes);
-router.use('/resultVideo', resultVideoRoutes);
+
+// router.use('/uploadVideo', uploadedVideoRoutes);
+// router.use('/resultVideo', resultVideoRoutes);
 
 module.exports = router;
