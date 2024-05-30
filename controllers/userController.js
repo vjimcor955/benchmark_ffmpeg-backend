d1da76ff9ad4bc77f7185b2dd0000835c675d7f6
vjimcor955 <vjimcor955@g.educaand.es>
2024-05-30 18:04:34 +0200

    userController completed

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,21 +2,32 @@
 
 const User = require('../models/User');
 
-exports.createUser = async (req, res) => {
+// Get all users
+exports.getAllUsers = async (req, res) => {
   try {
-    console.log(req.body)
-    const { name, user, email, password } = req.body;
-    const newUser = await User.create({ name, user, email, password });
-    res.status(201).json(newUser);
+    const users = await User.findAll();
+    res.status(200).json(users);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 };
 
-exports.getAllUsers = async (req, res) => {
+// Get user by id
+exports.getUserById = async (req, res) => {
   try {
-    const users = await User.findAll();
-    res.status(200).json(users);
+    const user = await User.findByPk(req.params.id);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// Delete user
+exports.deleteUser = async (req, res) => {
+  try {
+    const user = await User.findByPk(req.params.id);
+    await user.destroy();
+    res.status(204).json({ message: 'User deleted' });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
