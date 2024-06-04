18a381b7c9afe48e4d0b1e1217f2af4d1bfc3244
vjimcor955 <vjimcor955@g.educaand.es>
2024-06-04 20:14:11 +0200

    add cors

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ require('dotenv').config();
 const express = require('express');
 const { sequelize } = require('./models');
 const mainRouter = require('./routes'); // Import the main router
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/api', mainRouter); // Use the main router
 
