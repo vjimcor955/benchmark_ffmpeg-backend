29f0f33f009caaa5636eb45e2af71f7bea94c697
vjimcor955 <vjimcor955@g.educaand.es>
2024-06-08 12:37:53 +0200

    upload and result routes

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,15 @@
 const express = require('express');
 const userRoutes = require('./userRoutes');
 const uploadedVideoRoutes = require('./uploadedVideoRoutes');
-// const resultVideoRoutes = require('./resultVideoRoutes');
+const resultVideoRoutes = require('./resultVideoRoutes');
 
 const router = express.Router();
 
 // Use individual route modules
 router.use('/user', userRoutes);
 
-router.use('/uploadVideo', uploadedVideoRoutes);
+router.use('/upload', uploadedVideoRoutes);
 
-// router.use('/resultVideo', resultVideoRoutes);
+router.use('/result', resultVideoRoutes);
 
 module.exports = router;
